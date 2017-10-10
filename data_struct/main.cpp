c5bbfaa774da0672c1761eaa42368a661bfbdb2c
CmST0us <cmst0us@CmST0us-MacBook.local>
2017-10-10 20:23:13 +0800

    Initial Commit

---
+++ data_struct/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data_struct
+//
+//  Created by CmST0us on 2017/10/10.
+//  Copyright © 2017年 eric3u. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
